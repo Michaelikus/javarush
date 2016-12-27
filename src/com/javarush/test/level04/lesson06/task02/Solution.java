9cea18b82bd0f541d374d1f5e1bfecae70dd858e
Mikhail Butalin <m.butalin@1001tur.ru>
2016-12-27 14:43:43 +0300

    -

--- src/com/javarush/test/level04/lesson06/task02/Solution.java
+++ src/com/javarush/test/level04/lesson06/task02/Solution.java
@@ -12,21 +12,16 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] numbers = new int[4];
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+        int number3 = Integer.parseInt(reader.readLine());
+        int number4 = Integer.parseInt(reader.readLine());
 
-        for (int number_index = 0; number_index < 4; number_index++){
-            numbers[number_index] = Integer.parseInt(reader.readLine());
-            //System.out.println(numbers[number_index]);
-        }
+        int maximum = number1;
+        if(maximum <= number2) maximum = number2;
+        if(maximum <= number3) maximum = number3;
+        if(maximum <= number4) maximum = number4;
 
-        System.out.println("Maximum is " + MaxInt(numbers));
-    }
-    private static int MaxInt(int[] numbers_array){
-        int maximum;
-        maximum = numbers_array[0];
-        for (int number_index = 0; number_index < 4; number_index++){
-            if(numbers_array[number_index] > maximum ) maximum = numbers_array[number_index];
-        }
-        return maximum;
+        System.out.println(maximum);
     }
 }
